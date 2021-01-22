23efe6ff0882ab252c7d250e4dcd8809ffb0264b
hobosapiens <trudovoe.popov@gmail.com>
2021-01-22 22:43:25 +0300

    Добавлена обработка ошибок и кнопки слайдера рандомных персонажей

---
+++ src/components/error-indicator/error-indicator.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import './error-indicator.css';
+import error from './../../images/error.png';
+
+const ErrorIndicator = () => {
+    return (
+        <div className="error-indicator">
+            <div className="error-indicator-text">
+                <span className="boom">Oooooooops!</span>
+                <span>something go wrong</span>
+                <span>(but Morty already fixing it)</span>
+            </div>
+            <div className="error-indicator-img">
+                <img src={error} alt="error" />
+            </div>
+        </div>
+    )
+};
+
+export default ErrorIndicator;
--- src/components/random-character/random-character.js
+++ src/components/random-character/random-character.js
@@ -2,6 +2,7 @@ import React, {Component} from 'react';
 import './random-character.css';
 import Api from "../../services/api";
 import Preloader from "../preloader";
+import ErrorIndicator from "../error-indicator";
 
 export default class RandomCharacter extends Component {
 
@@ -9,57 +10,114 @@ export default class RandomCharacter extends Component {
 
     state = {
         character: {},
-        loading: true
+        loading: true,
+        prevBtnDisabled: false,
+        nextBtnDisabled: false
     };
 
-    constructor() {
-        super();
-        this.setCharacter();
+    componentDidMount() {
+        this.updateCharacter(this.randomId());
+        this.interval = setInterval(() => {
+            this.updateCharacter(this.randomId());
+            console.log('refresh');
+        }, 6000);
     }
 
-    randomId = Math.floor(Math.random() * (671 - 1) + 1);
+    componentWillUnmount() {
+        clearInterval(this.interval);
+    }
 
-    onCharacterLoad = (character) => {
+    randomId = () => {
+        return Math.floor(Math.random() * (671 - 1) + 1);
+    };
+
+    onCharacterLoaded = (character) => {
         this.setState({
             character,
+            loading: false,
+            error: false
+        })
+    };
+
+    onError = (err) => {
+        this.setState({
+            error: true,
             loading: false
         })
     };
 
-    setCharacter() {
+    updateCharacter = (id) => {
         this.ramApi
-            .getCharacter(this.randomId)
-            .then(this.onCharacterLoad)
-    }
+            .getCharacter(id)
+            .then(this.onCharacterLoaded)
+            .catch(this.onError);
+    };
+
+    prevCharacter = () => {
+        if(this.state.character.id <= 1){
+            this.setState({prevDisabled: true})
+        } else {
+            this.setState({prevDisabled: false});
+            this.updateCharacter(this.state.character.id - 1);
+            clearInterval(this.interval);
+        }
+    };
+
+    nextCharacter = () => {
+        if(this.state.character.id === 671){
+            this.setState({nextDisabled: true})
+        } else {
+            this.setState({nextDisabled: false});
+            this.updateCharacter(this.state.character.id + 1);
+            clearInterval(this.interval);
+        }
+    };
 
     render() {
-        const { character: {imgURL ,name, status, species, gender }, loading} = this.state;
-        const preloader =  loading ? <Preloader /> : null;
-        const avatar  =  loading ? null : <CharacterAvatar imgURL={imgURL} name={name} />;
+        const {character, loading, error, prevBtnDisabled, nextBtnDisabled} = this.state;
+        const hasData = !(loading || error);
+
+        const errorMessage = error ? <ErrorIndicator/> : null;
+        const preloader = loading ? <Preloader/> : null;
+        const content = hasData ? <CharacterContent character={character}
+                                                    prevBtnDisabled={prevBtnDisabled}
+                                                    nextBtnDisabled={nextBtnDisabled}
+                                                    prevCharacter={this.prevCharacter}
+                                                    nextCharacter={this.nextCharacter}/> : null;
 
         return (
             <div className="random-character col-lg-12 jumbotron">
-                <div className="random-character-photo jumbotron">
-                    {preloader}
-                    {avatar}
-                </div>
-                <div className="random-character-info">
-                    <ul className="list-group list-group-flush">
-                        <li className="list-group-item name"><span>Name: </span><span>{name}</span></li>
-                        <li className="list-group-item status"><span>Status: </span><span>{status}</span></li>
-                        <li className="list-group-item species"><span>Species: </span><span>{species}</span></li>
-                        <li className="list-group-item gender"><span>Gender: </span><span>{gender}</span></li>
-                    </ul>
-                </div>
+                {errorMessage}
+                {preloader}
+                {content}
             </div>
         )
     }
 };
 
-const CharacterAvatar = ({imgURL, name}) => {
+const CharacterContent = ({prevCharacter, nextCharacter, prevBtnDisabled, nextBtnDisabled, character: {imgURL, name, status, species, gender}}) => {
+    const prevBtnClass = prevBtnDisabled ? 'disabled' : '';
+    const nextBtnClass = nextBtnDisabled ? 'disabled' : '';
     return (
         <React.Fragment>
-            <img src={imgURL} alt={name} className="random-character-avatar" />
+            <div className="random-character-buttons">
+                <button className={'btn btn-outline-primary ' + prevBtnClass}
+                        onClick={prevCharacter}>PREV</button>
+                <button className={'btn btn-outline-primary ' + nextBtnClass}
+                        onClick={nextCharacter}>NEXT</button>
+            </div>
+            <div className="random-character-photo jumbotron">
+                <img src={imgURL} alt={name} className="random-character-avatar"/>
+            </div>
+            <div className="random-character-info">
+                <ul className="list-group list-group-flush">
+                    <li className="list-group-item name"><span>Name: </span><span>{name}</span></li>
+                    <li className="list-group-item status"><span>Status: </span><span>{status}</span></li>
+                    <li className="list-group-item species"><span>Species: </span><span>{species}</span></li>
+                    <li className="list-group-item gender"><span>Gender: </span><span>{gender}</span></li>
+                </ul>
+            </div>
         </React.Fragment>
+
     )
 };
