62fd4de9628277c275663247b92fbb390aa152ed
hobosapiens <trudovoe.popov@gmail.com>
2021-03-02 20:51:34 +0300

    Homepage text change

--- src/components/pages/home-page.js
+++ src/components/pages/home-page.js
@@ -7,7 +7,11 @@ const Homepage = () => {
         <React.Fragment>
             <RandomCharacter/>
             <div className="jumbotron text-center homepage">
-                <div className="homepage-text">Welcome to Rick And Morty cartoon database, have fun.</div>
+                <div className="homepage-text">
+                    Welcome to Rick And Morty cartoon database.<br/>
+                    Here you can find all the characters, locations and episodes of the series.<br/>
+                    You can search and count all the clones of Morty or Rick, or whatever. Have fun!
+                </div>
                 <div className="homepage-img">
                     <img src={rick} alt="rick"/>
                 </div>
